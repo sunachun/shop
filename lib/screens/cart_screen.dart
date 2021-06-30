859f188a1efd8fa97815b3924c0fd1c1ee50d05d
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-06-30 18:01:21 +0900

    cart_delete

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -12,7 +12,7 @@ class CartScreen extends StatelessWidget {
     final cart = Provider.of<Cart>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Your Cart'),
+        title: Text('Your Cart!'),
       ),
       body: Column(
         children: <Widget>[
