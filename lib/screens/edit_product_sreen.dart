5fefaf7ef404787b089d36dff5c257c9496b43db
suna0 <suna0.o4o4gym@ezweb.ne.jp>
2021-07-05 13:26:55 +0900

    更新

--- lib/screens/edit_product_sreen.dart
+++ lib/screens/edit_product_sreen.dart
@@ -95,14 +95,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
       _isLoading = true;
     });
     if (_editedProduct.id != null) {
-      Provider.of<Products>(
+      await Provider.of<Products>(
         context,
         listen: false,
       ).updateProduct(_editedProduct.id, _editedProduct);
-      setState(() {
-        _isLoading = false;
-      });
-      Navigator.of(context).pop();
     } else {
       try {
         await Provider.of<Products>(context, listen: false)
@@ -123,14 +119,19 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ],
           ),
         );
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-        Navigator.of(context).pop();
+        // } finally {
+        //   setState(() {
+        //     _isLoading = false;
+        //   });
+        //   Navigator.of(context).pop();
+        // }
       }
+      setState(() {
+        _isLoading = false;
+      });
+      Navigator.of(context).pop();
+      // Navigator.of(context).pop();
     }
-    // Navigator.of(context).pop();
   }
 
   @override
